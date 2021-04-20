a66f3333ba0036cf7e086b243e16e636629df1ef
“Millie <2310903m@student.gla.ac.uk>
2021-04-20 17:52:36 +0100

    fixed errors with HTML, venv etc

--- newsSite/latestNews/views.py
+++ newsSite/latestNews/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from newsSite.template import latestNews
 
 
 def index(request):
     context_dict = {'boldmessage': 'This just in...'}
-    return render(request, latestNews.index.html, context=context_dict)
+    return render(request, 'latestNews/index.html', context=context_dict)
 
 
 def stories(request):
