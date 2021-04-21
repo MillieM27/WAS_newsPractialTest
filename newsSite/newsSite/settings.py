dbaa39302f7f85ede02d27dda76af4ff319f1866
“Millie <2310903m@student.gla.ac.uk>
2021-04-21 12:15:36 +0100

    pythonanywhere - allowing host

--- newsSite/newsSite/settings.py
+++ newsSite/newsSite/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))#
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  #
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -26,8 +25,9 @@ SECRET_KEY = 'g%!hwj_a%jhxu5f0&t*m627yd*sdpxk32jmo3k(wqa=y95=#)7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['MillieM27.pythonanywhere.com/']
-
+ALLOWED_HOSTS = ['MillieM27.pythonanywhere.com/'
+                 '127.0.0.1'
+                 ]
 
 # Application definition
 
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'newsSite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -101,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
